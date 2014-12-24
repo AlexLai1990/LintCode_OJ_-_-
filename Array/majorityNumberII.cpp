4b0cd8ea2c43b52c309aa3a6c0f5f8566d4c4a8f
AlexLai1990 <xincheng.lai@gmail.com>
2014-12-23 19:06:50 -0800

    majorityNumber
    throw different number each time when size comes to k.

--- Array/majorityNumberII.cpp
+++ Array/majorityNumberII.cpp
@@ -8,17 +8,17 @@ public:
     /**
      * @param nums: A list of integers
      * @return: The majority number occurs more than 1/3.
-     */
-    int majorityNumber(vector<int> nums) {
-        // write your code here
+     */ 
+    
+    int majorityNumber(vector<int> nums) {  
     	unordered_map<int, int> m_dict;
-    	// int m_majority;
+    	int m_majority, m_count = INT_MIN;
     	for (int i = 0; i < nums.size(); i++) {
     		if (m_dict.find(nums[i]) != m_dict.end()) {
     			m_dict[nums[i]]++;
     		}
     		// what if nums[i] is not in m_dict
-    		else if (m_dict.size() == 2) {
+    		else if (m_dict.size() == 3) {
     			vector<unordered_map<int, int>::iterator> m_del;
     			for (auto it = m_dict.begin(); it != m_dict.end(); it++) {
     				if (--(it->second) == 0)
@@ -31,24 +31,15 @@ public:
     		else {
     			m_dict[nums[i]] = 1;
     		}
+    	} 
+    	
+    	for (auto it = m_dict.begin(); it != m_dict.end(); it++) {
+    	    if(m_count < it->second) {
+    	        m_count = it->second;
+    	        m_majority = it->first;
+    	    }
     	}
-    	if (m_dict.size() == 0)
-		    return m_dict.begin()->first;
-    	else {
-    		int m_majority, count = INT_MIN;
-    		for (auto it = m_dict.begin(); it != m_dict.end(); it++) { 
-    			int temp_count = 0;
-    			for (int i = 0; i < nums.size(); i++) {
-    				if (nums[i] == it->first)
-    					temp_count++;
-    				if (count < temp_count) {
-    					count = temp_count;
-    					m_majority = it->first; 
-    				}
-    			}
-		    }
-		    return m_majority;
-	    }
+    	return m_majority;
     }
 };
 
