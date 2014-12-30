b6d114e69a669005a3773d8fd2611d5fa36b9cf7
AlexLai1990 <xincheng.lai@gmail.com>
2014-12-29 16:55:13 -0800

    Merge Two Sorted Lists

---
+++ Linked List/Merge Two Sorted Lists.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition of ListNode
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param ListNode l1 is the head of the linked list
+     * @param ListNode l2 is the head of the linked list
+     * @return: ListNode head of linked list
+     */
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        // write your code here
+        ListNode dummy(-1);
+        ListNode *curr = &dummy;
+        while (l1 != NULL && l2 != NULL) {
+            if (l1->val < l2->val) {
+                curr->next = l1;
+                l1 = l1->next;
+            }
+            else {
+                curr->next = l2;
+                l2 = l2->next;
+            }
+            curr = curr->next;
+        }
+        curr->next = l1 != NULL ? l1 : l2;
+        return dummy.next;
+    }
+};
