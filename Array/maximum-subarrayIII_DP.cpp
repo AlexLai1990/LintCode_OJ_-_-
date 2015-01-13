ab2687c7601c68c4354303b69e9c08714935c51c
AlexLai1990 <xincheng.lai@gmail.com>
2015-01-12 17:11:22 -0800

    SubarrayIII

--- Array/maximum-subarrayIII_DP.cpp
+++ Array/maximum-subarrayIII_DP.cpp
@@ -14,29 +14,30 @@ public:
         int n = nums.size();
     	if (n <= 1 || k > nums.size())
     		return 0;
-    	// m_dp[i][j] means the largest number from 0 to j in nums having i subarray
+    	// m_dp[i][j] means the largest number when using nums[0..j] with i subarrays
     	vector<vector<int> > m_dp(k + 1, vector<int>(n + 1, INT_MIN));
     	fill(m_dp[0].begin(), m_dp[0].end(), 0); 
     	vector<int> pre_sum(n + 1, 0);
+		// pre-store a pre-sum array for get sum from nums[i] to num[j] later.
     	for (int i = 1; i <= n; i++) {
     		pre_sum[i] += pre_sum[i - 1] + nums[i - 1];
-    	}
+		}
     	for (int i = 1; i <= k; i++) { 
     		for (int j = i; j <= n; j++) {
     			if (i == j) {
     				m_dp[i][j] = pre_sum[i];
     				continue;
     			}
-    			// the nums[j] as the last subarray
-    			// m_dp[i][j] = m_dp[i - 1][j - 1] + nums[j - 1];
-    			// if cut in the middle
+				// get fake largest, define current largest is using num[j] as the last number in the ith subarray
+    			// try to find a cut from i to j
     			for (int k = i; k <= j ; k++) {
     				m_dp[i][j] = max(m_dp[i][j], m_dp[i - 1][k - 1] + pre_sum[j] - pre_sum[k - 1]);
     			}
     		}
-    		// update for this row
-    		for (int j = i + 1; j <= n; j++)
+    		// update the real largest number in current row
+    		for (int j = i + 1; j <= n; j++) {
     			m_dp[i][j] = max(m_dp[i][j], m_dp[i][j - 1]);
+			}
     	}
     	return m_dp[k][n];
     }
